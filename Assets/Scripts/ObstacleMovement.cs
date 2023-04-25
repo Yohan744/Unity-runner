3c985b4960366951c8dc535077424f55a9db142b
Yohan744 <73723399+Yohan744@users.noreply.github.com>
2023-04-25 10:58:04 +0200

    updated ground visibility, reduced grave's speed

--- Assets/Scripts/ObstacleMovement.cs
+++ Assets/Scripts/ObstacleMovement.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 
 public class ObstacleMovement : MonoBehaviour
 {
-    private float forwardForce = 350f;
+    private float forwardForce = 250f;
     [SerializeField] private Rigidbody rb;
 
     void FixedUpdate()
     {
         //Debug.Log("Force : " + (forwardForce + (Time.timeSinceLevelLoad * 13)) * Time.deltaTime);
-        rb.AddForce(new Vector3(0, 0, -(forwardForce + (Time.timeSinceLevelLoad * 15))* Time.deltaTime));
+        rb.AddForce(new Vector3(0, 0, -(forwardForce + (Time.timeSinceLevelLoad * 18))* Time.deltaTime));
 
     }
 }
